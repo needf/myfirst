b1c523c8001320164a333daa56a11ac2fd32ae22
needf <zy20130729@gmail.com>
2013-08-09 15:14:05 +0800

    创建类

---
+++ python/creat.py
@@ -0,0 +1,11 @@
+_metaclass_ =type #确定使用新类
+
+class Person:
+    
+    def setName(self, name):
+        self.name =name
+    def getname(sel:):
+	return self.name
+
+    def greet(self):
+	print "Hello, world! I'm %s,"% self.name
